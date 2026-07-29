9d659ac1f5f072c82b14a90a3a5a1b08b507e0ff
Renan Cavichi <renancavichi@gmail.com>
2026-07-28 22:11:57 -0300

    Update App.js greeting message and enhance package.json with additional dependencies

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá Mundo React Native! Rodando na Web</Text>
       <StatusBar style="auto" />
     </View>
   );
