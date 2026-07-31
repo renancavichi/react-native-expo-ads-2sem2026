d6fa42fe9b7baa7c58e9b442eb57d635f8574cc7
Renan Cavichi <renancavichi@gmail.com>
2026-07-30 22:20:47 -0300

    Add user card component with avatar, name, and email to App.js

--- App.js
+++ App.js
@@ -13,7 +13,33 @@ export default function App() {
         />
         <Text style={styles.textLogo}>APP Legal</Text>
       </View>
-      <View style={styles.box2}></View>
+      <View style={styles.box2}>
+      {/* //criar um card de usuário com avatar, nome e email */}
+      <View style={styles.cardUser}>
+        <Image
+          style={styles.userAvatar}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>John Doe</Text>
+          <Text style={styles.userEmail}>john.doe@example.com</Text>
+        </View>
+      </View>
+      <View style={styles.cardUser}>
+        <Image
+          style={styles.userAvatar}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>John Doe</Text>
+          <Text style={styles.userEmail}>john.doe@example.com</Text>
+        </View>
+      </View>
+      </View>
        <StatusBar style="auto" />
     </View>
   );
@@ -44,5 +70,40 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
     fontWeight: 'bold'
+  },
+  cardUser: {
+    width: '90%',
+    marginTop: 20,
+    marginHorizontal: 'auto',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  userAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  userInfo: {
+    marginLeft: 20,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  userEmail: {
+    fontSize: 14,
+    color: '#666',
   }
 });
