b48774d936cc1094d3cc75b683c822cad6a603fa
Renan Cavichi <renancavichi@gmail.com>
2026-07-30 21:54:58 -0300

    Refactor App.js layout and add README.md with evaluation topics

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo React Native! Rodando na Web</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <Text style={styles.textLogo}>APP Legal</Text>
+      </View>
+      <View style={styles.box2}></View>
+       <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +22,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffdf4f',
   },
+  box: {
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#345678',
+    padding: 20,
+    gap: 20
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: '#5fbb21'
+  },
+  tinyLogo: {
+    width: 60,
+    height: 60,
+  },
+  textLogo: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# Tópicos Importantes para Avaliação
+
+- Qual a diferença entre os pacotes react, react-native e expo?
+- No Stylesheet do React Native tenho todas as propriedades do CSS disponível?
